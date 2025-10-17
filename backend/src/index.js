60beee9553540b9848602519dea6766c0a35f3c0
aaronnguyen99 <nhoanghiep99@gmail.com>
2025-10-17 02:28:21 -0500

    fix bug

--- backend/src/index.js
+++ backend/src/index.js
@@ -25,7 +25,7 @@ routes(app);
 app.use(express.static(path.join(__dirname, 'dist')));
 
 // Catch-all: redirect all unknown routes to index.html
-app.get(/.*/, (req, res) => {
+app.get(/^(?!\/api).*/, (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 // --- API route example ---
